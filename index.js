5e8c9e7cd7871e0c37131002ecaa68b004699909
Slade Hirschi <sladehirsc@gmail.com>
2022-09-27 21:38:20 -0600

    Removed sql connection for now

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const nodemailer = require('nodemailer');
 const mysql = require('mysql');
 const path = require('path');
 
-const con = mysql.createConnection({
-	host: process.env.DATABASE_HOST,
-	user: process.env.DATABASE_USERNAME,
-	password: process.env.DATABASE_PASSWORD,
-});
+// const con = mysql.createConnection({
+// 	host: process.env.DATABASE_HOST,
+// 	user: process.env.DATABASE_USERNAME,
+// 	password: process.env.DATABASE_PASSWORD,
+// });
 
-con.connect(function (err) {
-	if (err) {
-		console.error('Database connection failed: ' + err.stack);
-		return;
-	}
-	console.log("Connected!");
-});
+// con.connect(function (err) {
+// 	if (err) {
+// 		console.error('Database connection failed: ' + err.stack);
+// 		return;
+// 	}
+// 	console.log("Connected!");
+// });
 
 const app = express()
 
